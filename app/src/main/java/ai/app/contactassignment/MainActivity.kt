062ba71c8210b7f2a388d98972de097f9753a5a9
hj9993019161 <harsh.jain@conquerapps.com>
2024-09-27 11:05:53 +0530

    harsh assignment

--- app/src/main/java/ai/app/contactassignment/MainActivity.kt
+++ app/src/main/java/ai/app/contactassignment/MainActivity.kt
@@ -28,7 +28,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Toast.makeText(this, "merging data new branch" , Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "merging data new_branch_1" , Toast.LENGTH_SHORT).show()
 
         val recyclerViews = findViewById<RecyclerView>(R.id.recyclerView)
         val searchView = findViewById<SearchView>(R.id.searchView)
